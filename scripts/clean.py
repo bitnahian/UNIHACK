e70c0131bef8e2c6d70e97467d45ac943bbb108d
Shenin <sfai4579@uni.sydney.edu.au>
2018-08-04 17:30:18 +1000

    removed useless files

--- scripts/clean.py
+++ scripts/clean.py
@@ -5,10 +5,8 @@ df = pd.read_csv('../data/cleaned_data.csv')
 df_speed = df.loc[df['SPEED_IND'] == 'Y']
 df_red = df.loc[df['RED_LIGHT_CAMERA_IND'] == 'Y']
 
-df_speed = df_speed[['LOCATION_CODE', 'LOCATION_DETAILS', 'CAMERA_TYPE',
-    'SCHOOL_ZONE_IND', 'SPEED_IND']]
-df_red = df_red[['LOCATION_CODE', 'LOCATION_DETAILS', 'CAMERA_TYPE',
-    'SCHOOL_ZONE_IND', 'RED_LIGHT_CAMERA_IND']]
+df_speed = df_speed[['LOCATION_CODE', 'LOCATION_DETAILS', 'CAMERA_TYPE','SCHOOL_ZONE_IND', 'SPEED_IND']]
+df_red = df_red[['LOCATION_CODE', 'LOCATION_DETAILS', 'CAMERA_TYPE','SCHOOL_ZONE_IND', 'RED_LIGHT_CAMERA_IND']]
 
 df_speed_counts = df_speed["LOCATION_CODE"].value_counts()
 df_red_counts = df_red["LOCATION_CODE"].value_counts()
@@ -17,8 +15,11 @@ df_speed = df_speed.groupby('LOCATION_CODE').agg({'CAMERA_TYPE':'first','LOCATIO
 
 df_red = df_red.groupby('LOCATION_CODE').agg({'CAMERA_TYPE':'first','LOCATION_DETAILS':'first','SCHOOL_ZONE_IND':'first', 'RED_LIGHT_CAMERA_IND':'first'}).reset_index()
 
-df_speed_counts = df_speed_counts.to_frame().reset_index(level=0,inplace=True)
-df_red_counts = df_red_counts.to_frame().reset_index(level=0,inplace=True)
+df_speed_counts = df_speed_counts.to_frame()
+df_speed_counts.reset_index(level=0,inplace=True)
+
+df_red_counts = df_red_counts.to_frame()
+df_red_counts.reset_index(level=0,inplace=True)
 
 df_speed_counts.columns=['LOCATION_CODE', 'COUNT']
 df_red_counts.columns=['LOCATION_CODE', 'COUNT']
@@ -26,3 +27,5 @@ df_red_counts.columns=['LOCATION_CODE', 'COUNT']
 df_speed_final = pd.merge(df_speed, df_speed_counts, how='inner', on='LOCATION_CODE')
 df_red_final = pd.merge(df_red, df_red_counts, how='inner', on='LOCATION_CODE')
 
+df_speed_final.to_csv('../data/speed_data.csv',index=False)
+df_red_final.to_csv('../data/red_light_data.csv',index=False)
